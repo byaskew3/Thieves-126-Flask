8a6abeac8c10ccfa610a34ff262daae67315b1d9
byaskew3 <byaskew3@gmail.com>
2023-08-18 11:54:18 -0600

    Database Implementation + AUTH

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,15 @@
-from flask import request, render_template
+from flask import request, render_template, flash, redirect, url_for
 import requests
 from app import app
 from .forms import LoginForm, SignupForm
+from flask_login import login_user, logout_user, current_user, login_required
+from .models import User, db
+from werkzeug.security import check_password_hash
 
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('home.html')
-
-# fake database
-REGISTERED_USERS = {
-    'dylank@thieves.com': {
-        'name': 'Dylan Katina',
-        'password': 'ilovemydog'
-    }
-}
 
 # HTTP Methods & Rendering Templates
 @app.route('/login', methods=['GET', 'POST'])
@@ -23,28 +18,45 @@ def login():
     if request.method == 'POST' and form.validate_on_submit():
         email = form.email.data.lower()
         password = form.password.data
-        if email in REGISTERED_USERS and password == REGISTERED_USERS[email]['password']:
-            return f'Hello, {REGISTERED_USERS[email]["name"]}'
+        # query the user object from database
+        queried_user = User.query.filter(User.email == email).first()
+        if queried_user and check_password_hash(queried_user.password, password):
+            login_user(queried_user)
+            flash(f'Hello, {queried_user.first_name}!', 'primary')
+            return redirect(url_for('home'))
         else:
-            return 'Invalid email or password'
+            flash('Invalid email or password', 'danger')
+            return redirect(url_for('login'))
     else:
-        print('not validated')
         return render_template('login.html', form=form)
     
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
     if request.method == 'POST' and form.validate_on_submit():
-        name = f'{form.first_name.data} {form.last_name.data}'
-        email = form.email.data
+
+        # Grabbing our sign up form data
+        first_name = form.first_name.data 
+        last_name = form.last_name.data
+        email = form.email.data.lower()
         password = form.password.data
-        REGISTERED_USERS[email] = {
-            'name': name,
-            'password': password
-        }
-        return f'Thank you for registering {name}!'
+        
+        # Creating an instance of the User Model
+        new_user = User(first_name, last_name, email, password)
+
+        # Adding new user to our database
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash(f'Thank you for signing up {new_user.first_name}!', 'primary')
+        return redirect(url_for('login'))
     else:
         return render_template('signup.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 @app.route('/students')
 def students():
@@ -54,7 +66,8 @@ def students():
 
 
 @app.route('/f1/driverStandings', methods=['GET', 'POST'])
-def get_driver_info_year_rnd():
+@login_required
+def driver_standings():
     if request.method == 'POST':
         year = request.form.get('year')
         rnd = request.form.get('rnd')
